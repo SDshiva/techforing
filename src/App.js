1b736d4d03588a3d11cf11f253752351b15dae79
SDshiva <shibam.dey.sd@gmail.com>
2023-04-07 14:08:34 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
-import { AppBar, Button, Grid, TextField } from '@mui/material';
+import { AppBar, Button, Grid } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <AppBar>
-        <Grid container>
-          <Grid item md={8}>
-            <h3>Tech Foring</h3>
-            <small>Shaping Tomorrows Cybersecurity</small>
-          </Grid>
-          <Grid item md={4}>
+      <header>
+        <AppBar>
+          <Grid container spacing={2}>
+            <Grid item md={8}>
+              <h3>Tech Foring</h3>
+              <p><small>Shaping Tomorrows Cybersecurity</small></p>
+            </Grid>
+            <Grid item md={4}>
 
-            <Button variant='outlined' color='error'>Sign Up</Button>
-            <Button variant="contained" color="success">Sign In</Button>
+              <Button variant='outlined' color='error'>Sign Up</Button>
+              <Button variant="contained" color="success">Sign In</Button>
 
+            </Grid>
           </Grid>
-        </Grid>
-      </AppBar>
-      <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </div>
+        </AppBar>
+      </header>
+      
+
+
     </div>
   );
 }
